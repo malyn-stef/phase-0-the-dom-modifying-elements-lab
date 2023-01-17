8bcba1d9f73efd177db257ade2d433d9fd5fcf2c
+ <+>
2023-01-17 16:48:17 -0700

    completed dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const removeMain = document.getElementById("main")
+removeMain.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Malyn Steffey is the champion";
+document.body.append(newHeader);
+
+
+
+
+
